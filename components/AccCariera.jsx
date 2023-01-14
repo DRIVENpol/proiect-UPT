162c426c382acde41292427f7b9b937bda1676fc
Socarde Paul <84129217+DRIVENpol@users.noreply.github.com>
2023-01-14 03:14:51 +0200

    Cariera

---
+++ components/AccCariera.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Box, Center, Image, VStack, Text, HStack, Button,
+    Accordion,
+    AccordionItem,
+    AccordionButton,
+    AccordionPanel,
+    AccordionIcon } from '@chakra-ui/react'
+    import Link from 'next/link'
+
+const AccCariera = () => {
+  return (
+   <>
+   <Accordion width={'100%'}>
+  <AccordionItem>
+    <h2>
+      <AccordionButton>
+        <Box as="span" flex='1' textAlign='left'>
+         <Text as='b'>Rev3al</Text>
+        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+    </h2>
+    <AccordionPanel pb={4}>
+    <Text as='b'>Descriere</Text>
+        <Text>Rev3al este un proiect blockchain care are ca scop protejareaq asseturilor digitale de tip NFT. Acestia pun la dispozitie un API care va manipula
+        metadata unui NFT astfel incat acesta nu poate fi replicat.</Text><br />
+        <Text as='b'>Ce am realizat pentru ei?</Text>
+        <Text>Un ERC20 smart contract cu un mecanism de staking virtual integrat. Practic, utilizatorii pot pune monede la stake fara a le trimite
+            unui contract de tip MasterChef - monedele puse la stake sunt vizibile in portofel dar nu pot fi transferate.
+            De altfel s-a realizat si o interfata web care sa comunice cu smart contractul ERC20 si cu un alt smart contract pentru vesting.
+        </Text><br />
+        <Button colorScheme='messenger'><Link href='https://medium.com/@psocarde/rev3al-migration-behind-the-scenes-part-1-6f09e7a0ce02' target='_blank'>Citeste mai mult</Link></Button>
+    </AccordionPanel>
+  </AccordionItem>
+
+  <AccordionItem>
+    <h2>
+      <AccordionButton>
+        <Box as="span" flex='1' textAlign='left'>
+        <Text as='b'>Metarix</Text>
+        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+    </h2>
+    <AccordionPanel pb={4}>
+    <Text>Metarix este o solutie blockchain de tip L2(Layer 2) care a fost construita pe baza technologiei ZK RollUps. Aceasta doreste sa se focuseze
+        mai mult pe partea de gaming a blockchainului si sa ajute dezvoltatorii oferind diverse SDK-uri.</Text><br />
+        <Text as='b'>Ce am realizat pentru ei?</Text>
+        <Text>Un smart contract pentru staking, farming, NFT market place si DEX (Decentralized Exchange) pentru care ulterior se va construi si interfete web aferente.
+        </Text><br />
+        <Button colorScheme='messenger'><Link href='https://metarix.network/' target='_blank'>Vezi Website</Link></Button>
+    </AccordionPanel>
+  </AccordionItem>
+</Accordion>
+   </>
+  )
+}
+
+export default AccCariera
--- pages/cariera.jsx
+++ pages/cariera.jsx
@@ -1,8 +1,69 @@
+import { Box, Center, Image, VStack, Text, HStack,
+ } from '@chakra-ui/react'
+import Link from 'next/link'
 import React from 'react'
+
+import pozaProfil from '../images/pozaProfil.jpeg'
+import fbIcon from '../images/facebook.png'
+import twitterIcon from '../images/twitter.png'
+import linkedIcon from '../images/linkedin.png'
+import gitIcon from '../images/github.png'
+import AccCariera from '@/components/AccCariera'
 
 const cariera = () => {
   return (
-    <div>cariera</div>
+   <>
+   <Center>
+    <VStack width={'100%'} m='20' gap='10'>
+    <Box mb='10'><VStack>
+        <Image
+        borderRadius='full'
+        boxSize='150px'
+        src={pozaProfil.src}
+        alt='Dan Abramov'
+        />
+    <Text as='b' color='white'>Paul Socarde | Varsta: 24</Text>
+    <Text as='b' color='white'>📍Timisoara, Romania</Text>
+
+    <HStack>
+    <Link href='https://www.facebook.com/socardepaul1/' target="_blank"><Image src={fbIcon.src} height='24px' width={'24px'} /></Link>
+    <Link href='https://twitter.com/polthedev' target="_blank"><Image src={twitterIcon.src} height='24px' width={'24px'} /></Link>
+    <Link href='https://www.linkedin.com/in/socardepaul/' target="_blank"><Image src={linkedIcon.src} height='24px' width={'24px'} /></Link>
+    <Link href='https://github.com/DRIVENpol' target="_blank"><Image src={gitIcon.src} height='24px' width={'24px'} /></Link>
+
+    </HStack>
+    </VStack>
+    </Box>
+
+    <Box borderRadius='xl' bgColor={'white'} w='100%' h={'100%'} p='10'>
+    <Center>
+    <VStack>
+    <Text as='b' fontSize={'4xl'}>💻 Loc de munca actual</Text>
+    <Text fontSize={'xl'} align='center'>
+    In momentul de fata nu am un rol bine stabilit in firma mea. Fiind la inceput si avand un deficit de resurse umane, taskurile mele sunt urmatoarele:
+    organizare, dezvoltarea de produse noi, managementul echipei de developeri, audite de securitate. Pe langa acestea, particip frecvent la hackathons si 
+    bug-bounties.
+    </Text>
+    </VStack>
+    </Center>
+    </Box>
+
+    <Box borderRadius='xl' bgColor={'white'} w='100%' h={'100%'} p='10'>
+    <Center>
+    <VStack>
+    <Text as='b' fontSize={'4xl'}>🔮 Proiecte interesante</Text>
+    <Text fontSize={'xl'} align='center'>
+    Ultimele doua proiecte realizate cu succes:
+    </Text>
+    <AccCariera />
+    </VStack>
+    </Center>
+    </Box>
+
+    </VStack>
+    
+    </Center>
+    </>
   )
 }
 
